7e3944e8a9558419de5af54815e7fe2e77f31662
Guy Cherkesky <guy@cherkesky.com>
2020-08-27 14:55:50 -0500

    initial commit

---
+++ lib/my_project.ex
@@ -0,0 +1,22 @@
+defmodule MyProject do
+  @moduledoc """
+  Documentation for `MyProject`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MyProject.hello()
+      :world
+
+  """
+  def add(a,b) do
+    a + b
+  end
+
+  def substract(a,b) do
+    a - b
+  end
+end
---
+++ test/my_project_test.exs
@@ -0,0 +1,19 @@
+defmodule MyProjectTest do
+  use ExUnit.Case
+  doctest MyProject
+
+  # test "greets the world" do
+  #   assert MyProject.hello() == :world
+  # end
+
+  test "adding numbers" do
+    assert MyProject.add(1, 1) == 2
+    assert MyProject.add(3, 2) == 5
+  end
+
+  test "substracting numbers" do
+    assert MyProject.substract(4, 2) == 2
+    assert MyProject.substract(3, 2) == 1
+  end
+
+end
